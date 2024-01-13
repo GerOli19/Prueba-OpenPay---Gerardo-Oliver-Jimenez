c41e9744f82815526bc4aecb308317f1f58d9be0
Gerardo Oliver Jiménez <goliverj@liverpool.com.mx>
2024-01-13 13:46:57 -0600

    Commit Ultimas Correcciones Conummer Api Marvel.

--- SearchMarvelCharacter/src/main/java/com/search/marvel/SearchMarvelApplication.java
+++ SearchMarvelCharacter/src/main/java/com/search/marvel/SearchMarvelApplication.java
@@ -3,7 +3,7 @@ package com.search.marvel;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-/*
+/**
 * @auto Gerardo Oliver Jiménez
 * */
 @SpringBootApplication
